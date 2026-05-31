230d8259f151dd2f00613099f7a21357517c3c72
Dev-Jhuvel <devjhuvel@gmail.com>
2026-05-31 21:45:28 +0800

    adjust tech animation

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
-import AOS from 'aos';
+import AOS from "aos";
 import useAOSRefresh from "../useAOSRefresh";
 
-const Technology = ({tech, size, id = 0}) =>{
-    const [hoveredTech, setHoveredTech] = useState(false);
-    useAOSRefresh(hoveredTech);
-    return (
-        <i 
-            data-aos="flip-up"
-            data-aos-delay={200 * id}
-            className={`text-${size} transition-transform duration-500 ease-in-out hover:scale-130 max-sm:text-2xl devicon-${tech} ${hoveredTech ? 'colored': ''}`}
-            onMouseOver={()=> setHoveredTech(true)} 
-            onMouseOut={()=> setHoveredTech(false)}
-            ></i>
-    )
-}
+const Technology = ({ tech, size, id = 0 }) => {
+  const [hoveredTech, setHoveredTech] = useState(false);
+  useAOSRefresh(hoveredTech);
+  const technology = `devicon-${tech} ${hoveredTech ? "colored" : ""}`;
+  const delay = 200 * id;
+  return (
+    <div data-aos="flip-up" data-aos-delay={delay} className="inline-block">
+      <i
+        className={`text-${size} transition-transform duration-500 ease-in-out hover:scale-130 max-sm:text-2xl ${technology} `}
+        onMouseOver={() => setHoveredTech(true)}
+        onMouseOut={() => setHoveredTech(false)}
+      ></i>
+    </div>
+  );
+};
 
 export default Technology;
